7f603d869ef6ce3332ad5df973fa8240a7b826e0
Yuqiy77 <kellyc5766836@gmail.com>
2025-08-11 23:06:18 -0400

    chore: disable eslint/ts checks for deploy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // 忽略构建时 ESLint 错误
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // 构建时忽略 ESLint
   },
   typescript: {
-    // 忽略 TypeScript 类型错误
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // 构建时忽略 TS 错误
   },
 };
 
